7b8e020a4bf6a4d2835ed80a9e464b614bd57787
allen.li <allen014996@gmail.com>
2015-12-16 16:44:15 +0800

    test commit from ssh by android studio

--- app/src/main/java/com/allen/test/test/MainActivity.java
+++ app/src/main/java/com/allen/test/test/MainActivity.java
@@ -32,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void test1(){
+
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
